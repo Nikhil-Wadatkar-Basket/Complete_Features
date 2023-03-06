8239a6d5235bd6b01e327484e77ae9f2187c6661
Nikhil-Wadatkar-Basket <wadatkarnikhil22@gmail.com>
2023-03-06 13:56:48 +0530

    comment added

--- employees-service/src/main/java/com/bs/EmployeesServiceApplication.java
+++ employees-service/src/main/java/com/bs/EmployeesServiceApplication.java
@@ -2,7 +2,7 @@ package com.bs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//starter class
 @SpringBootApplication
 public class EmployeesServiceApplication {
 
